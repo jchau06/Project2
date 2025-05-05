755be33f9d8a5bd449059c54db5087f280383b12
Jonathan Chau <chaujt1@uci.edu>
2025-05-05 00:02:47 -0700

    Refactored Engine class. Engine stores dictionary ._handlers storing what functions should handle specific events. process_event searches the dictionary and yields the result from a function.

--- p2app/engine/main.py
+++ p2app/engine/main.py
@@ -8,8 +8,7 @@
 # This is the outermost layer of the part of the program that you'll need to build,
 # which means that YOU WILL DEFINITELY NEED TO MAKE CHANGES TO THIS FILE.
 import sqlite3
-from pathlib import Path
-from p2app.events import OpenDatabaseEvent, DatabaseOpenedEvent, DatabaseOpenFailedEvent
+from p2app.events import OpenDatabaseEvent, DatabaseOpenedEvent, DatabaseOpenFailedEvent, CloseDatabaseEvent, DatabaseClosedEvent, QuitInitiatedEvent, EndApplicationEvent
 
 
 class Engine:
@@ -22,6 +21,9 @@ class Engine:
     def __init__(self):
         """Initializes the engine"""
         self._connection = None
+        self._handlers = {OpenDatabaseEvent: self._handle_open_database,
+                          CloseDatabaseEvent: self._handle_close_database,
+                          QuitInitiatedEvent: self._handle_quit_application}
 
 
     def process_event(self, event):
@@ -31,12 +33,24 @@ class Engine:
         # This is a way to write a generator function that always yields zero values.
         # You'll want to remove this and replace it with your own code, once you start
         # writing your engine, but this at least allows the program to run.
-        if isinstance(event, OpenDatabaseEvent):
-            try:
-                db_path = event.path()
-                self._connection = sqlite3.connect(db_path)
-                yield DatabaseOpenedEvent(db_path)
-            except Exception as e:
-                yield DatabaseOpenFailedEvent(str(e))
-        else:
-            yield from ()
+        handler = self._handlers.get(type(event), self._handle_unrecognized)
+        yield from handler(event)
+
+    def _handle_open_database(self, event):
+        try:
+            self._connection = sqlite3.connect(event.path())
+            yield DatabaseOpenedEvent(event.path())
+        except Exception as e:
+            yield DatabaseOpenFailedEvent(str(e))
+
+    def _handle_close_database(self, event):
+        if self._connection:
+            self._connection.close()
+            self._connection = None
+        yield DatabaseClosedEvent()
+
+    def _handle_quit_application(self, event):
+        yield EndApplicationEvent()
+
+    def _handle_unrecognized(self, event):
+        yield from ()
